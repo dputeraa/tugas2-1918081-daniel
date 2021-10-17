a13110832bd57dbb5f8f81d86c7bbcf32e2f0793
dputeraa <danielputeraalamsyah085@gmail.com>
2021-10-17 21:27:29 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+"# prak2-1918081-daniel"
+"# tugas2-1918081-daniel" 
+"# tugas2-1918081-danielputeraalamsyah" 
+"# tugas2-1918081-danielputeraalamsyah" 
+"# tugas2-1918081-daniel" 
+"# tugas2-1918081-daniel" 
---
+++ app/src/main/java/com/example/fragment_1918081/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.fragment_1918081;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fragment_1918081.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
